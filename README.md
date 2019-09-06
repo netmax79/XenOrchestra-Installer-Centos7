adbae3db20b638095ddca44921e76519a067d668
Carsten Schoene <cs@linux-administrator.com>
2019-09-06 11:02:05 +0200

    - change color variables - drop external issue.net integration - add no_banner patch

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # XenOrchestra-Installer-Centos7
+
 Use curl / wget
-run "curl -L https://raw.githubusercontent.com/Adepurnomo/XenOrchestra-Installer-Centos7/master/XOA-Installer-Centos7.sh | sh"
-run "wget -O- https://raw.githubusercontent.com/Adepurnomo/XenOrchestra-Installer-Centos7/master/XOA-Installer-Centos7.sh | sh"
+
+run "curl -L https://raw.githubusercontent.com/netmax79/XenOrchestra-Installer-Centos7/master/XOA-Installer-Centos7.sh | sh"
+
+run "wget -O- https://raw.githubusercontent.com/netmax79/XenOrchestra-Installer-Centos7/master/XOA-Installer-Centos7.sh | sh"
 
--- XOA-Installer-Centos7.sh
+++ XOA-Installer-Centos7.sh
@@ -1,90 +1,92 @@
 #!/bin/bash
 ##
-merah=$(tput setaf 1)
-hijau=$(tput setaf 2)
-kuning=$(tput setaf 3)
-echo "${hijau}######################################"
-echo "${hijau}Please run this scripts on SU"
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+orange=$(tput setaf 3)
+echo "${green}######################################"
+echo "${green}Please run this scripts on SU"
 echo "######################################"
 hostnamectl set-hostname XOA
-echo "${hijau}==================================="
-echo "${hijau}Working...."
-echo "${hijau}Please wait.."
-echo "${hijau}==================================="
+echo "${green}==================================="
+echo "${green}Working...."
+echo "${green}Please wait.."
+echo "${green}==================================="
 yum install epel-release -y > /dev/null 2>&1
 #yum update -y > /dev/null 2>&1
 # install ssl
-echo "${hijau}==================================="
+echo "${green}==================================="
 echo "Install Open SSL.."
-echo "${hijau}==================================="
+echo "${green}==================================="
 yum install mod_ssl -y > /dev/null 2>&1
 # add repo node js
-echo "${merah}==================================="
+echo "${red}==================================="
 echo "add repo nodeJS v8.xx..."
-echo "${merah}==================================="
+echo "${red}==================================="
 curl -s -L https://rpm.nodesource.com/setup_8.x | bash - > /dev/null 2>&1
 curl -s -o /etc/yum.repos.d/yarn.repo https://dl.yarnpkg.com/rpm/yarn.repo > /dev/null 2>&1
 # Node
-echo "${merah}==================================="
+echo "${red}==================================="
 echo "install nodeJS...."
 echo "Please wait......"
-echo "${merah}==================================="
+echo "${red}==================================="
 sleep 1
 yum install nodejs -y  > /dev/null 2>&1
 # install yarn package
-echo "${hijau}==================================="
+echo "${green}==================================="
 echo "Install yarn package...."
-echo "${hijau}==================================="
+echo "${green}==================================="
 sleep 2
 yum install yarn -y > /dev/null 2>&1
 # install lib vhd tools
 sleep 1
-echo "${merah}==================================="
+echo "${red}==================================="
 echo "Install vhd tools...."
-echo "${merah}==================================="
+echo "${red}==================================="
 rpm -ivh https://forensics.cert.org/cert-forensics-tools-release-el7.rpm > /dev/null 2>&1
 sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/cert-forensics-tools.repo > /dev/null 2>&1
 yum --enablerepo=forensics install -y libvhdi-tools > /dev/null 2>&1
 # install kebutuhan xoa
-echo "${hijau}==================================="
+echo "${green}==================================="
 echo "Install tool 4 xoa..."
-echo "${hijau}==================================="
+echo "${green}==================================="
 sleep 1
-yum install gcc gcc-c++ make openssl-devel redis libpng-devel python git nfs-utils -y > /dev/null 2>&1
+yum install gcc gcc-c++ make openssl-devel patch redis libpng-devel python git nfs-utils -y > /dev/null 2>&1
 # enable service redis  dll 
-echo "${kuning}==================================="
+echo "${orange}==================================="
 echo "enable redis server..."
-echo "${hijau}==================================="
+echo "${green}==================================="
 /bin/systemctl enable redis && /bin/systemctl start redis
 /bin/systemctl enable rpcbind && /bin/systemctl start rpcbind 
-echo "${kuning}==================================="
+echo "${orange}==================================="
 echo " clone xoa engine from source"
-echo "${kuning}==================================="
+echo "${orange}==================================="
 sleep 1
 cd /opt/
 /usr/bin/git clone https://github.com/vatesfr/xen-orchestra 
 # allow config restoreee
 sed -i 's/< 5/> 0/g' /opt/xen-orchestra/packages/xo-web/src/xo-app/settings/config/index.js
-echo "${kuning}==================================="
-echo "Build your XOA ..."
-echo "${hijau}==================================="
+echo "${orange}==================================="
+echo "Build your XOA ... (in 10 seconds)"
+echo "${green}==================================="
 sleep 10
-echo "${kuning}==================================="
+echo "${orange}==================================="
 echo "3 ..."
-echo "${hijau}==================================="
-echo "${kuning}==================================="
+echo "${green}==================================="
+echo "${orange}==================================="
 echo "2 ..."
-echo "${hijau}==================================="
-echo "${kuning}==================================="
+echo "${green}==================================="
+echo "${orange}==================================="
 echo "1 ..."
-echo "${hijau}==================================="
+echo "${green}==================================="
 cd /opt/xen-orchestra
+curl -s -L -o /root/no_banner.diff https://raw.githubusercontent.com/netmax79/xen-orchestra-ce/master/alpine/patches/no_banner.diff
+patch -p1 < /root/no_banner.diff
 /usr/bin/yarn
 /usr/bin/yarn build
 # configure xoa
-echo "${hijau}==================================="
+echo "${green}==================================="
 echo "----------AUTO configure xoa----------"
-echo "${kuning}==================================="
+echo "${orange}==================================="
 sleep 5
 cd /opt/xen-orchestra/packages/xo-server
 \cp sample.config.toml .xo-server.toml
@@ -96,30 +98,17 @@ sed -i "s|# port = 443|port = 443|" .xo-server.toml
 sed -i "s|# cert = './certificate.pem'|cert = '/root/cert/cert-selfsigned.pem'|" .xo-server.toml
 sed -i "s|# key = './key.pem'|key = '/root/cert/key-selfsigned.pem'|" .xo-server.toml
 # create node
-echo "${kuning}==================================="
+echo "${orange}==================================="
 echo "Create node ..."
-echo "${hijau}==================================="
+echo "${green}==================================="
 sleep 5
 mkdir -p /usr/local/lib/node_modules/
 ln -s /opt/xen-orchestra/packages/xo-server-* /usr/local/lib/node_modules/
 rm -rf /etc/systemd/system/xo-server.service
-# Cow BANNER
-echo "${hijau}==================================="
-echo "attach kebo banner :v"
-echo "${hijau}==================================="
-sleep 3
-cd /root/
-git clone https://github.com/Adepurnomo/test.git
-\cp /root/test/issue.net /etc
-chmod a+x /etc/issue.net
-cd /etc/ssh/ 	
-sed -i "s|#Banner none|Banner /etc/issue.net|" sshd_config
-chmod a+x etc/ssh/sshd_config
-rm -rf /root/test
 #bikin service daemon xoa
-echo "${kuning}==================================="
+echo "${orange}==================================="
 echo "Create XOA daemon service on systemd ....."
-echo "${hijau}==================================="
+echo "${green}==================================="
 /bin/cat << EOF >> /etc/systemd/system/xo-server.service
 # Systemd service for XO-Server.
 
@@ -137,18 +126,17 @@ SyslogIdentifier=xo-server
 WantedBy=multi-user.target
 EOF
 
-echo "${kuning}==================================="
+echo "${orange}==================================="
 echo "... SELFSIGN-SSL Auto configure for XOA ..."
-echo "${hijau}==================================="
+echo "${green}==================================="
 sleep 5
-mkdir /root/cert
-chmod 700 /root/cert
-echo "${kuning}==================================="
+mkdir -p /etc/ssl/cert
+echo "${orange}==================================="
 echo "Generate self ssl"
-echo "${kuning}==================================="
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /root/cert/key-selfsigned.pem -out /root/cert/cert-selfsigned.pem -subj "/C=Id/ST=DKI Jakarta/L=Jakarta/O=Ade Purnomo/OU=IT Department/CN=Port of Tanjung Priok"
-openssl dhparam -out /root/cert/dhparam.pem 2048
-cat /root/cert/dhparam.pem | tee -a /etc/ssl/certs/cert-selfsigned.pem
+echo "${orange}==================================="
+openssl req -x509 -nodes -days 3650 -newkey rsa:4096 -keyout /etc/ssl/cert/key-selfsigned.pem -out /etc/ssl/cert/cert-selfsigned.pem -subj "/C=DE/ST=Bavaria/L=Munich/O=Linux Administrator Networks/OU=IT Department/CN=xoa"
+openssl dhparam -out /etc/ssl/cert/dhparam.pem 4096
+cat /etc/ssl/cert/dhparam.pem | tee -a /etc/ssl/certs/cert-selfsigned.pem
 echo "+++++++++++++++++++++++++++"
 echo "========="DONE"============"
 echo "+++++++++++++++++++++++++++"
@@ -168,16 +156,16 @@ node -v
 npm -v
 yarn --version
 sleep 2
-echo "${hijau}==================================="
+echo "${green}==================================="
 echo "done"
-echo "${hijau}==================================="
-echo "${kuning}==================================="
+echo "${green}==================================="
+echo "${orange}==================================="
 host=$(hostname -I)
 echo "and then acces https://$host"
 echo "username : admin@admin.net"
 echo "password : admin"
 #echo "follow my ig @nextorchestra / @pauziah_collection"
-echo "${kuning}================================================="
+echo "${orange}================================================="
 service sshd restart
 echo "--------------------------------------------------------------"
 
